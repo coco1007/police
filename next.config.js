a526126c6fe47b6abd3f560e08be7a376dafb637
coco1007 <kimeunsoo.com@gmail.com>
2025-05-11 13:45:20 +0900

    Remove swcMinify option from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': __dirname,
-    };
-    return config;
+  output: 'standalone',
+  images: {
+    domains: ['images.unsplash.com'],
   },
 }
 
